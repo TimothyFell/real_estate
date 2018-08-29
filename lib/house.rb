118a0c6d25639572c657518f932b623f9bcda302
TimothyFell <timothy.fell846@gmail.com>
2018-08-29 13:45:11 -0600

    Expand house class to Iteration 3 specs

--- lib/house.rb
+++ lib/house.rb
@@ -15,4 +15,16 @@ class House
     @rooms << room
   end
 
+  def rooms_from_category(room_category)
+    @rooms.select do |room|
+      room.category == room_category
+    end
+  end
+
+  def area
+    @rooms.sum do |room|
+      room.area
+    end
+  end
+
 end
